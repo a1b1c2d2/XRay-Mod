853c58e9eb055fdc32fdf7c6af2d5b8d7c454f04
Michael Hillcox <michaelhillcox@gmail.com>
2018-08-06 08:13:22 +0100

    More logical way of storing crap

--- src/main/java/com/xray/common/reference/BlockStore.java
+++ src/main/java/com/xray/common/reference/BlockStore.java
@@ -1,14 +1,40 @@
 package com.xray.common.reference;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.ResourceLocation;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BlockStore {
 
-    private List<Integer> 
+    private HashMap<String, Integer> drawableBlocks = new HashMap<>();
+    private HashMap<String, BlockData> storedBlocks = new HashMap<>();
+
+    public boolean storeBlock( int blockStateId, BlockData data ) {
+        for (Map.Entry<String, BlockData> dataStore:
+             storedBlocks.entrySet()) {
+
+            if(Block.getStateId(dataStore.getValue().getState()) == blockStateId )
+                return false;
+        }
+
+        this.drawableBlocks.put("UID HERE", blockStateId);
+        this.storedBlocks.put("SAME UID HERE", data);
+        return true;
+    }
+
+    public boolean storeContainsDefault(IBlockState state) {
+        for (Map.Entry<String, BlockData> dataStore:
+                storedBlocks.entrySet()) {
+
+            if(dataStore.getValue().getState() == state.getBlock().getDefaultState() )
+                return true;
+        }
+        return false;
+    }
 
 }
